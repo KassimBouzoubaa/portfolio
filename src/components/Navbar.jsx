4f2641ff46d2ef5f0609aa802961c76017557f3d
Kassim <kassimbouzoubaa05@gmail.com>
2023-03-22 19:18:27 +0100

    5

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt="Logo Image" style={{width: '70px'}} />
+            <img src={Logo} alt="Logo Image" style={{width: '100px'}} />
         </div>
 
         {/* Menu */}
