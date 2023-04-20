4e0ca6511b0ec8c23dae730c2714c1e30e1bfa64
Kassim <kassimbouzoubaa05@gmail.com>
2023-04-20 03:25:11 +0200

    ajout

--- src/data/data.js
+++ src/data/data.js
@@ -11,6 +11,14 @@ export const data=[
         image:Krypto,
         github:"https://github.com/KassimBouzoubaa/Krypto",
         live:"https://krypto-five.vercel.app/",
+        description:"Crypto Tutor est un blog éducatif sur la blockchain développé avec Next.js, React, Tailwind CSS, GraphQL et GraphCMS. Le blog offre un contenu informatif sur la blockchain, avec des sujets tels que les concepts fondamentaux, les tendances du marché, et les cas d'utilisation. Le site web est rapide, interactif et offre une expérience utilisateur fluide grâce à Next.js et React. Le design moderne et réactif est réalisé avec Tailwind CSS. GraphQL est utilisé pour gérer efficacement les données du blog, tandis que GraphCMS est utilisé comme système de gestion de contenu convivial pour la création et la publication de contenu. Crypto Tutor est un projet innovant visant à informer et éduquer les lecteurs sur la blockchain en utilisant les dernières technologies web pour offrir une expérience utilisateur optimale."
+    },
+    {
+        id:1,
+        name:"Crypto Tutor",
+        image:tutor,
+        github:"https://github.com/KassimBouzoubaa/crypto_tutor",
+        live:"https://crypto-tutor.vercel.app/",
         description:"Cette application offre la possibilité d'envoyer des ethers sur le réseau de test ethereum (goerli) tout en affichant les dernières transactions en temps réel. Elle est connectée à la blockchain et permet d'interagir avec des smart contracts tout en connectant son portefeuille Metamask. Elle a été développée en utilisant les technologies React/TailwindCSS ainsi que solidity/hardhat/ethersjs."
     },
     {
